261d88a36aa2812740010993e765c06d7eea672b
Khushivraj23 <khushivraj2003@gmail.com>
2025-05-07 13:42:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,4 +190,7 @@ def api_report_data():
 
 # ---------------------- RUN FLASK APP ----------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
